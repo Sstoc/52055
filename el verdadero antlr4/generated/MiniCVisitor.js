956cbfc69292db148d94077d112a479cd4cbffa9
Andrés Moreno <aandres.moreno3@gmail.com>
2025-05-18 11:51:38 -0300

    Subir carpeta 'el verdadero antlr4'

---
+++ el verdadero antlr4/generated/MiniCVisitor.js
@@ -0,0 +1,76 @@
+// Generated from c:/Users/aandr/OneDrive/Documentos/el verdadero antlr4/MiniC.g4 by ANTLR 4.13.1
+// jshint ignore: start
+import antlr4 from 'antlr4';
+
+// This class defines a complete generic visitor for a parse tree produced by MiniCParser.
+
+export default class MiniCVisitor extends antlr4.tree.ParseTreeVisitor {
+
+	// Visit a parse tree produced by MiniCParser#programa.
+	visitPrograma(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by MiniCParser#instrucciones.
+	visitInstrucciones(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by MiniCParser#instruccion.
+	visitInstruccion(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by MiniCParser#bucle.
+	visitBucle(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by MiniCParser#sentencia.
+	visitSentencia(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by MiniCParser#salida.
+	visitSalida(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by MiniCParser#terminar.
+	visitTerminar(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by MiniCParser#condicion.
+	visitCondicion(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by MiniCParser#cadena.
+	visitCadena(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by MiniCParser#caracteres.
+	visitCaracteres(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by MiniCParser#caracter.
+	visitCaracter(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+
+}
